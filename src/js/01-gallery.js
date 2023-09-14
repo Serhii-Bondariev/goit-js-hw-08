0896f7aba1ac377e651b8ed12bab31332b9dd422
Serhii-Bondariev <xx.code4u@gmail.com>
2023-09-14 21:43:31 +0300

    a

---
+++ src/js/01-gallery.js
@@ -0,0 +1,12 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+const user = {
+    name: 'John Smith',
+    age: 20,
+}
+
+console.log(user);
