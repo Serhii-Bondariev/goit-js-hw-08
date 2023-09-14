73bf43cc8f24300da25e7b7993183b8f78037cb4
Serhii-Bondariev <xx.code4u@gmail.com>
2023-09-14 22:16:55 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,5 +5,29 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+function murkup(gallery) {
+  return gallery
+    .map((item) => {
+      console.log(item);
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+       <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+ </li>`;
+    })
+    .join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", murkup(galleryItems));
+console.log(murkup(galleryItems));
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+  captionPosition: "bottom",
+});
+
 
 console.log(galleryItems);
